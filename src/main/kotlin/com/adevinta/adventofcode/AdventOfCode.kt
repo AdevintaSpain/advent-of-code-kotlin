f532ba243cba39051cf581ef6571de4cb5ee7ac4
Yanina Morales <yanigmorales@gmail.com>
2021-12-14 22:18:43 +0100

    algunas mejoras?

--- src/main/kotlin/com/adevinta/adventofcode/AdventOfCode.kt
+++ src/main/kotlin/com/adevinta/adventofcode/AdventOfCode.kt
@@ -1,5 +1,83 @@
 package com.adevinta.adventofcode
 
-class AdventOfCode {
-	fun saySomething() = "hello!"
+import kotlin.system.exitProcess
+
+enum class Result {
+    COMPLETE,
+    INCOMPLETE,
+    CORRUPTED
 }
+
+class AdventOfCode {
+    fun saySomething() = "hello!"
+
+    fun syntaxScoring(firstInput: String): Boolean {
+
+        var parentesis = 0
+        var llave = 0
+        var masmenos = 0
+        var corchete = 0
+
+
+        firstInput.forEach {
+            when (it) {
+                '(' -> parentesis += 1
+                ')' -> parentesis -= 1
+                '{' -> llave += 1
+                '}' -> llave -= 1
+                '<' -> masmenos += 1
+                '>' -> masmenos -= 1
+                '[' -> corchete += 1
+                ']' -> corchete -= 1
+            }
+
+            if (parentesis < 0 || llave < 0 || masmenos < 0 || corchete < 0) return false
+        }
+
+        return parentesis == 0 && llave == 0 && masmenos == 0 && corchete == 0
+
+    }
+}
+
+enum class characterçtype(simbol: Char){
+    PARENTESIS('(')
+
+
+
+
+}
+
+data class Character(
+    val open: Int = 0,
+    val close: Int = 0
+) {
+    companion object {
+        fun incress(character: Character) = Character(
+            open = character.open + 1
+        )
+
+        fun decress(character: Character): Character {
+            if (character.close - 1 < 0) throw exitProcess(0)
+            return Character(close = character.close - 1)
+        }
+    }
+}
+
+
+/*
+data class character(
+    val open: Int = 0,
+    val close: Int = 0
+) {
+    fun incress() {
+        this.copy(open = open + 1)
+    }
+
+    fun decress() {
+        if (close -1 < 0) throw exitProcess(0)
+        this.copy(close = close -1)
+    }
+}
+
+
+ */
--- src/test/kotlin/com/adevinta/adventofcode/AdventOfCodeTest.kt
+++ src/test/kotlin/com/adevinta/adventofcode/AdventOfCodeTest.kt
@@ -1,12 +1,38 @@
 package com.adevinta.adventofcode
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
+
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class AdventOfCodeTest {
+
+  private val adventoOfCode = AdventOfCode()
 
   @Test
   fun `should call advent of code`() {
     assertThat(AdventOfCode().saySomething()).isEqualTo("hello!")
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = ["()", "[]"])
+  internal fun `search syntax scoring`(input: String) {
+
+    val result = adventoOfCode.syntaxScoring(input)
+
+    assertThat(result).isTrue
+
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = ["())", "[[]", ")(", "{([(<{}[<>[]}>{[]{[(<()>"])
+  internal fun `search incorrect syntax scoring`(input: String) {
+
+    val result = adventoOfCode.syntaxScoring(input)
+
+    assertThat(result).isFalse
+
   }
 }
