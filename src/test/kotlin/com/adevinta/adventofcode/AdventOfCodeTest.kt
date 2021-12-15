6ad44ac88aa815b0e424587c817d9f6b23c9d045
Yanina Morales <yanigmorales@gmail.com>
2021-12-15 22:03:03 +0100

    un enter? Bendito enter

--- src/test/kotlin/com/adevinta/adventofcode/AdventOfCodeTest.kt
+++ src/test/kotlin/com/adevinta/adventofcode/AdventOfCodeTest.kt
@@ -2,11 +2,25 @@ package com.adevinta.adventofcode
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class AdventOfCodeTest {
 
-  @Test
-  fun `should call advent of code`() {
-    assertThat(AdventOfCode().saySomething()).isEqualTo("hello!")
-  }
+    @ParameterizedTest
+    @CsvSource(value =
+    [
+        "28;11, 1",
+        "21;81, 1",
+        "5, 0",
+        "1;1, 0",
+        "11, 0",
+        "111, 1",
+        "639;521;581, 6"
+    ])
+    fun `find lowest risk`(input: String, expectedResult: Int) {
+        val result = AdventOfCode().findLowestRisk(input.split(";"))
+
+        assertThat(result).isEqualTo(expectedResult)
+    }
 }
